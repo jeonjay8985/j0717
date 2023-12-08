ae746a758f6ce58819079b525f9e0c2441fa9964
yj-603-19 <yj-603-19@DESKTOP-4I99M6O>
2023-12-08 12:03:47 +0900

    j0717

---
+++ src/j0717/exam01.java
@@ -0,0 +1,90 @@
+/*
+작성일 :2023.07.17
+작성자:
+자바 예제
+*/ 
+
+package j0717;
+
+public class exam01{
+
+	
+	public static void main(String[] args) {
+		int num1=100;
+		int num2=50;
+		int result=num1+num2;
+
+		System.out.printf("%d+%d=%d\n", num1, num2, result); //더하기
+		result=num1-num2;
+		System.out.printf("%d-%d=%d\n", num1, num2, result); //빼기
+		result=num1*num2;
+		System.out.printf("%d*%d=%d\n", num1, num2, result); //곱하기
+		result=num1/num2;
+		System.out.printf("%d/%d=%d\n", num1, num2, result); //나누기
+		
+		System.out.println(num1==101);
+		System.out.println(num1==100);
+		
+		/*
+	int n1=10;
+	int n2=3;
+	float f1;
+	fl=n1;
+	
+	System.out.println("F1 : "(int)f1);
+	
+	System.out.println(n1/n2);
+	
+	System.out.println((float)n1/n2);
+	
+	int i;
+	double f;
+	
+	f=5/4;
+	System.out.println(f);
+	
+	f=(double)5/4;
+	System.out.println(f);
+	
+	f=(int)1.3 + (int)1.8; //1.3정수로->1 1.8정수로->1 =2
+	System.out.println(f);
+	*/
+	
+	
+	
+	
+		
+		
+		
+		
+	}
+}
+
+		
+
+		
+		
+		
+		
+		
+	
+	
+			
+		
+	
+		
+		
+		
+		
+
+
+		
+		
+		
+	
+
+		
+		
+		
+		
+
